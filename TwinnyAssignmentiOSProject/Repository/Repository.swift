02bab6eb46d0e971adc189c540d14dc76a12d1a6
ehdudgnsTW <brown@twinny.co.kr>
2022-04-14 19:40:05 +0900

    Add Repository file
    api통신을 하기전에 목업 데이터를 생성하기 위해서 MockRepository class를 생성하였고
    Repository class는 실제로 api통신을 하기위해서 만들어 놓은 클래스 입니다.
    그리고 이둘은 같은 형식을 가져야 한다고 생각을 해서 프로토콜을 만들어서 진행하였습니다.
    
    RPD-2865

---
+++ TwinnyAssignmentiOSProject/Repository/Repository.swift
@@ -0,0 +1,69 @@
+//
+//  Repository.swift
+//  TwinnyAssignmentiOSProject
+//
+//  Created by twinny on 2022/04/14.
+//
+
+import Foundation
+import RxSwift
+
+protocol RepositoryProtocol {
+    var totalData: [FavoriteDataModel] { get set }
+    func getTotalData() -> Observable<[FavoriteDataModel]>
+    func getCSVFileToData() -> Observable<[FavoriteDataModel]>
+}
+
+final class Repository: RepositoryProtocol {
+    var totalData: [FavoriteDataModel] = []
+    
+    func getTotalData() -> Observable<[FavoriteDataModel]> {
+        return .just(totalData)
+    }
+    
+    func getCSVFileToData() -> Observable<[FavoriteDataModel]> {
+        return .just(totalData)
+    }
+}
+
+final class MockRepository: RepositoryProtocol {
+    let currentTemperature: [Float] = [12,16,18,10,14,13,11,20]
+    let maxTemperature: [Float] = [32,24,20,21,23]
+    let minTemperature: [Float] = [10,9,8,7,6,5]
+    let favorite: [Bool] = [true, false]
+
+    var totalData: [FavoriteDataModel] = []
+
+    func getCSVFileToData() -> Observable<[FavoriteDataModel]>{
+        var XYData: [String:[String]] = [:]
+        let path = Bundle.main.path(forResource: "기상청41_단기예보-조회서비스_오픈API활용가이드_격자_위경도_20210401_", ofType: "csv")!
+        do {
+            let data = try Data(contentsOf: URL(fileURLWithPath: path))
+            let dataEncoded = String(data: data, encoding: .utf8)
+            if let dataArr = dataEncoded?.components(separatedBy: "\n").map({
+                $0.components(separatedBy: "\",\"")
+            }) {
+                for i in 1..<dataArr.count {
+                    if dataArr[i][3] != "", dataArr[i][4] != "" {
+                        let tmpStr = "\(dataArr[i][2]) \(dataArr[i][3]) \(dataArr[i][4])"
+                        XYData[tmpStr] = [dataArr[i][4],dataArr[i][5]]
+                        totalData.append(FavoriteDataModel(currentTemperature: currentTemperature[Int.random(in: 0..<currentTemperature.count)],
+                                                           maxTemperature: maxTemperature[Int.random(in: 0..<maxTemperature.count)],
+                                                           minTemperature: minTemperature[Int.random(in: 0..<minTemperature.count)],
+                                                           cityId: dataArr[i][1],
+                                                           isFavoriet: favorite[Int.random(in: 0..<2)],
+                                                           cityName: tmpStr))
+                    }
+                }
+            }
+        } catch {
+            print("reading csv file error")
+        }
+        return .just(totalData)
+    }
+    
+    func getTotalData() -> Observable<[FavoriteDataModel]> {
+        return .just(totalData)
+    }
+}
+        
