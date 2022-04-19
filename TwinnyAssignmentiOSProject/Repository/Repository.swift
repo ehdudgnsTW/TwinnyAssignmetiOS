33b8917152cc349d66ef662ca455a5ba4f0b07fb
ehdudgnsTW <brown@twinny.co.kr>
2022-04-19 11:57:52 +0900

    Refactor Repository file
    기존에 메모리에다가 목업 데이터를 만들어서 앱을 실행할때 마다 데이터를 생성하였다면 지금은 초기 목업 데이터 값들을 json파일로 생성한뒤에 json파일을 읽고 수정 하는식으로 변경 하였습니다.
    
    RPD-2867

--- TwinnyAssignmentiOSProject/Repository/Repository.swift
+++ TwinnyAssignmentiOSProject/Repository/Repository.swift
@@ -11,31 +11,58 @@ import RxSwift
 protocol RepositoryProtocol {
     var totalData: [FavoriteDataModel] { get set }
     func getTotalData() -> Observable<[FavoriteDataModel]>
-    func getCSVFileToData() -> Observable<[FavoriteDataModel]>
+    func changeTotalData(totoalData: [FavoriteDataModel])
 }
 
 final class Repository: RepositoryProtocol {
+    
     var totalData: [FavoriteDataModel] = []
     
-    func getTotalData() -> Observable<[FavoriteDataModel]> {
-        return .just(totalData)
+    func changeTotalData(totoalData: [FavoriteDataModel]) {
+        
     }
     
-    func getCSVFileToData() -> Observable<[FavoriteDataModel]> {
+    func getTotalData() -> Observable<[FavoriteDataModel]> {
         return .just(totalData)
     }
 }
 
 final class MockRepository: RepositoryProtocol {
-    let currentTemperature: [Float] = [12,16,18,10,14,13,11,20]
-    let maxTemperature: [Float] = [32,24,20,21,23]
-    let minTemperature: [Float] = [10,9,8,7,6,5]
-    let favorite: [Bool] = [true, false]
-
+    
     var totalData: [FavoriteDataModel] = []
-
-    func getCSVFileToData() -> Observable<[FavoriteDataModel]>{
-        var XYData: [String:[String]] = [:]
+    
+    func changeTotalData(totoalData: [FavoriteDataModel]) {
+        let encoder = JSONEncoder()
+        encoder.outputFormatting = .prettyPrinted
+        let jsonData = try? encoder.encode(totoalData)
+        if let data = jsonData, let documentDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let path = documentDirectory.appendingPathComponent("citys")
+            do {
+                try data.write(to: path)
+            } catch {
+                print("error")
+            }
+        }
+    }
+    
+    func getTotalData() -> Observable<[FavoriteDataModel]> {
+        let decoder = JSONDecoder()
+        if let documenDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first {
+            let path = documenDirectory.appendingPathComponent("citys")
+            if let data = try? Data(contentsOf: path), let tmpData = try? decoder.decode([FavoriteDataModel].self, from: data) {
+                self.totalData = tmpData
+            }
+        }
+        return .just(totalData)
+    }
+    
+    //혹시 데이터가 날라갔을때 사용하기 위해서 만들어놓은 함수 입니다. 현재 앱에서는 json파일 만들때 사용하고 그뒤로는 사용을 하고 있지 않습니다.
+    func getCSVFileInit() {
+        
+        let currentTemperature: [Float] = [12,16,18,10,14,13,11,20]
+        let maxTemperature: [Float] = [32,24,20,21,23]
+        let minTemperature: [Float] = [10,9,8,7,6,5]
+        
         let path = Bundle.main.path(forResource: "기상청41_단기예보-조회서비스_오픈API활용가이드_격자_위경도_20210401_", ofType: "csv")!
         do {
             let data = try Data(contentsOf: URL(fileURLWithPath: path))
@@ -46,24 +73,23 @@ final class MockRepository: RepositoryProtocol {
                 for i in 1..<dataArr.count-1 {
                     if dataArr[i][3] != "", dataArr[i][4] != "" {
                         let tmpStr = "\(dataArr[i][2]) \(dataArr[i][3]) \(dataArr[i][4])"
-                        XYData[tmpStr] = [dataArr[i][4],dataArr[i][5]]
-                        totalData.append(FavoriteDataModel(currentTemperature: currentTemperature[Int.random(in: 0..<currentTemperature.count)],
-                                                           maxTemperature: maxTemperature[Int.random(in: 0..<maxTemperature.count)],
-                                                           minTemperature: minTemperature[Int.random(in: 0..<minTemperature.count)],
-                                                           cityId: dataArr[i][1],
-                                                           isFavoriet: false,
-                                                           cityName: tmpStr))
+                        if let xInt = Int(dataArr[i][5]), let yInt = Int(dataArr[i][6]) {
+                            totalData.append(FavoriteDataModel(currentTemperature: currentTemperature[Int.random(in: 0..<currentTemperature.count)],
+                                                               maxTemperature: maxTemperature[Int.random(in: 0..<maxTemperature.count)],
+                                                               minTemperature: minTemperature[Int.random(in: 0..<minTemperature.count)],
+                                                               cityId: dataArr[i][1],
+                                                               isFavoriet: false,
+                                                               cityName: tmpStr,
+                                                               locationCoordinate: CGPoint(x: xInt, y: yInt)))
+                                         
+                        }
                     }
                 }
             }
         } catch {
             print("reading csv file error")
         }
-        return .just(totalData)
-    }
-    
-    func getTotalData() -> Observable<[FavoriteDataModel]> {
-        return .just(totalData)
+        changeTotalData(totoalData: totalData)
     }
 }
         
