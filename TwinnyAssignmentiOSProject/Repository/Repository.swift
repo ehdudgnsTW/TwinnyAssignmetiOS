1f6e3dc22d50bac97e4f264e22bfd5566a8ffb6e
ehdudgnsTW <brown@twinny.co.kr>
2022-04-20 11:32:48 +0900

    Add json decode error case
    json 파일을 읽어올때 실패할 경우에 대한 상황에 대해서 다시 json파일을 덮어씌우는 작업을 추가 하였습니다.
    
    RPD-2868

--- TwinnyAssignmentiOSProject/Repository/Repository.swift
+++ TwinnyAssignmentiOSProject/Repository/Repository.swift
@@ -51,8 +51,48 @@ final class MockRepository: RepositoryProtocol {
             let path = documenDirectory.appendingPathComponent("citys")
             if FileManager.default.fileExists(atPath: path.path) {
                 if let data = try? Data(contentsOf: path), let tmpData = try? decoder.decode([FavoriteDataModel].self, from: data) {
-                    self.totalData = tmpData                }
+                    self.totalData = tmpData
+                }
+                else {
+                    let currentTemperature: [Float] = [12,16,18,10,14,13,11,20]
+                    let maxTemperature: [Float] = [32,24,20,21,23]
+                    let minTemperature: [Float] = [10,9,8,7,6,5]
+                    let encoder = JSONEncoder()
+                    encoder.outputFormatting = .prettyPrinted
+                    
+                    let csvPath = Bundle.main.path(forResource: "기상청41_단기예보-조회서비스_오픈API활용가이드_격자_위경도_20210401_", ofType: "csv")!
+                    do {
+                        let data = try Data(contentsOf: URL(fileURLWithPath: csvPath))
+                        let dataEncoded = String(data: data, encoding: .utf8)
+                        if let dataArr = dataEncoded?.components(separatedBy: "\n").map({ $0.components(separatedBy: "\",\"")}) {
+                            for cityData in dataArr {
+                                if cityData.count > 1, cityData[0].contains("kor") {
+                                    if cityData[3] != "", cityData[4] != "" {
+                                        let cityName = "\(cityData[2]) \(cityData[3]) \(cityData[4])"
+                                        if let x = Int(cityData[5]),let y = Int(cityData[6]) {
+                                            totalData.append(FavoriteDataModel(currentTemperature: currentTemperature[Int.random(in: 0..<currentTemperature.count)],
+                                                                     maxTemperature: maxTemperature[Int.random(in: 0..<maxTemperature.count)],
+                                                                     minTemperature: minTemperature[Int.random(in: 0..<minTemperature.count)] ,
+                                                                     cityId: cityData[1],
+                                                                     isFavorite: false,
+                                                                     cityName: cityName,
+                                                                     locationCoordinate: CGPoint(x: x, y: y)))
+                                        }
+
+                                    }
+                                }
+                            }
+                        }
+                        let jsonData = try? encoder.encode(self.totalData)
+                        if let data = jsonData {
+                            try data.write(to: path)
+                        }
+                    } catch {
+                        print("csvfile read error")
+                    }
+                }
             }
+                
             else {
                 let currentTemperature: [Float] = [12,16,18,10,14,13,11,20]
                 let maxTemperature: [Float] = [32,24,20,21,23]
