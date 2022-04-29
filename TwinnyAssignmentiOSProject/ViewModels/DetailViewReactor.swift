15a101a2d44dad24c09e1bcaad28fd1e7f1bf76d
ehdudgnsTW <brown@twinny.co.kr>
2022-04-29 10:44:16 +0900

    Refactor DetailViewReactor
    실제 기상데이터를 가져와서 업데이트를 하기 위한 action을 추가 하였습니다.
    업데이트가 완료 되었을때 토스트 메세지에 들어가는 메세지 갱신했습니다.
    
    RPD-2866

--- TwinnyAssignmentiOSProject/ViewModels/DetailViewReactor.swift
+++ TwinnyAssignmentiOSProject/ViewModels/DetailViewReactor.swift
@@ -11,24 +11,28 @@ import RxSwift
 
 class DetailViewReactor: Reactor {
     
-    private var repository = MockRepository.shared
+    private var repository = Repository.shared
+    private var disposeBag = DisposeBag()
     
     enum Action {
         case changeFavorite
+        case updateData
     }
     
     enum Mutation {
         case changeFavoriteState
+        case getmodelData(FavoriteDataModel, String)
     }
     
     struct State {
         var dataModel: FavoriteDataModel
+        var message: String
     }
     
     var initialState: State
     
     init(model: FavoriteDataModel) {
-        self.initialState = State(dataModel: model)
+        self.initialState = State(dataModel: model, message: "업데이트 중")
     }
     
     func mutate(action: Action) -> Observable<Mutation> {
@@ -36,6 +40,11 @@ class DetailViewReactor: Reactor {
         case .changeFavorite:
             repository.changeData(initialState.dataModel.cityId)
             return .just(.changeFavoriteState)
+        case .updateData:
+            repository.upDateDetailData(model: initialState.dataModel)
+            return repository.detailDataSubject.map {
+                DetailViewReactor.Mutation.getmodelData($0,"업데이트 완료!!")
+            }
         }
     }
     
@@ -44,6 +53,11 @@ class DetailViewReactor: Reactor {
         switch mutation {
         case .changeFavoriteState:
             state.dataModel.isFavorite.toggle()
+            state.message = "즐겨찾기 상태 변경 완료"
+            return state
+        case .getmodelData(let model, let message):
+            state.dataModel = model
+            state.message = message
             return state
         }
     }
